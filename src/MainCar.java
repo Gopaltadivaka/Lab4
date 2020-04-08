414bca040cef60a21e51ab3172ffa6d439dc732c
gopal <gopal@LAPTOP-THDP34FM.attlocal.net>
2020-04-08 14:56:23 -0400

    Demonstration of class

---
+++ src/MainCar.java
@@ -0,0 +1,7 @@
+
+public class MainCar {
+	public static void main(String[]args) {
+		Car  myCar=new Car();
+		//Demonstrate Usage of class
+		}
+}
---
+++ src/MainCircle.java
@@ -0,0 +1,7 @@
+
+public class MainCircle {
+	public static void main(String[]args) {
+	Circle  myCircle=new Circle();
+	//Demonstrate Usage of class
+	}
+}
